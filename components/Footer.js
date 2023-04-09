076fa15e186fb5ae81f9bfd90a8f2f0bcebec0e4
gulhancukuryurt <gcukuryurt@gmail.com>
2023-04-09 17:14:19 +0300

    footer add

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import styles from '../styles/Footer.module.css'
+import {FaPizzaSlice,FaCaretRight,FaPhoneAlt,FaInstagram,FaYoutube,FaFacebook} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        
+        <ul>
+          <li><FaPizzaSlice color='#f8bd11'/> Home</li>
+          <li><FaPizzaSlice color='#f8bd11'/> Product</li>
+          <li><FaPizzaSlice color='#f8bd11'/> Menu</li>
+          <li><FaPizzaSlice color='#f8bd11'/> Blog</li>
+          <li><FaPizzaSlice color='#f8bd11'/> Contact</li>
+        </ul>
+      </div>
+      <div className={styles.menudetails}>
+        <h2 className={styles.titleMenu}>MENU</h2>
+          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
+          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
+          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
+          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
+      </div>
+      <div className={styles.brands}>
+        <h2 className={styles.title}> BRANDS</h2>
+        <p className={styles.brand}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p className={styles.brand}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p className={styles.brand}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div className={styles.social}>
+        <div className={styles.contact}>
+      <div className={styles.contactmsg}>
+            ORDER NOW
+          </div>
+             <div className={styles.number}>
+              <FaPhoneAlt fontSize={"15px"}/> 444 55 66
+             </div>
+             </div>
+             <div className={styles.socialmedia}>
+               <h2><FaInstagram  className={styles.icon}/></h2>
+               <h2><FaYoutube className={styles.icon}/></h2>
+               <h2><FaFacebook className={styles.icon}/></h2>
+             </div>
+      </div>
+    </div>
   )
 }
 

86793c825d912855c4e4df70f3dc7ee3774c204d
gulhancukuryurt <gcukuryurt@gmail.com>
2023-06-12 14:26:52 +0300

    contact page add

---
+++ pages/contact.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styles from '../styles/ContactP.module.css'
+import { FaRegPaperPlane, FaMapMarkerAlt, FaPhoneAlt, FaRegEnvelope } from "react-icons/fa";
+import { FaInstagram, FaYoutube, FaFacebook } from "react-icons/fa";
+
+
+function contact() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.contact}>
+                <div className={styles.left}>
+                    <div className={styles.address}>
+                        <div className={styles.main}>
+
+                            <div className={styles.iconContainer}>
+                                <FaMapMarkerAlt style={{ fontSize: "1.3rem" }} />
+                                <label>Address</label>
+                            </div>
+                            <div className={styles.iconContainer}>
+                                <FaPhoneAlt style={{ fontSize: "1.3rem" }} />
+                                <label>444 55 66</label>
+                            </div>
+                            <div className={styles.iconContainer}>
+                                <FaRegEnvelope style={{ fontSize: "1.3rem" }} />
+                                <label>info@pizza.com</label>
+                            </div>
+                            <hr></hr>
+                            <div className={styles.socialmedia}>
+                                <h2><FaInstagram className={styles.icon} /></h2>
+                                <h2><FaYoutube className={styles.icon} /></h2>
+                                <h2><FaFacebook className={styles.icon} /></h2>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <form className={styles.form}>
+                        <input className={styles.input} type='text' placeholder='Name'></input>
+                        <input className={styles.input} type='email' placeholder='E-mail'></input>
+                        <input className={styles.input} type='text' placeholder='Subject'></input>
+                        <textarea className={styles.input} placeholder='Message'></textarea>
+                        <button type='submit' className={styles.btn}>Send Message <FaRegPaperPlane /></button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default contact

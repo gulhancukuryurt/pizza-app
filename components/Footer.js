be245e78b5cb0387a4366fd85c7f740a383d2de6
gulhancukuryurt <gcukuryurt@gmail.com>
2023-10-04 16:16:11 +0300

    footer updated

--- components/Footer.js
+++ components/Footer.js
@@ -1,58 +1,80 @@
-import React from 'react'
-import Link from 'next/link';
-import styles from '../styles/Footer.module.css'
-import {FaPizzaSlice,FaCaretRight,FaPhoneAlt,FaInstagram,FaYoutube,FaFacebook} from "react-icons/fa";
+import React, { useState } from "react";
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+import {
+  FaPizzaSlice,
+  FaCaretRight,
+  FaPhoneAlt,
+  FaInstagram,
+  FaYoutube,
+  FaFacebook,
+} from "react-icons/fa";
+
 
 const Footer = () => {
+  const [msg, setMsg] = useState("");
+
   return (
     <div className={styles.container}>
+     
       <div className={styles.menu}>
-        
         <ul>
           <Link href="/">
-          <li><FaPizzaSlice color='#f8bd11'/> Home</li>
+            <li>
+              <FaPizzaSlice className={styles.icon} color="#f8bd11" /> Home
+            </li>
           </Link>
           <Link href="/menu">
-          <li><FaPizzaSlice color='#f8bd11'/> Menu</li>
+            <li>
+              <FaPizzaSlice className={styles.icon} color="#f8bd11" /> Menu
+            </li>
           </Link>
           <Link href="/blog">
-          <li><FaPizzaSlice color='#f8bd11'/> Blog</li>
+            <li>
+              <FaPizzaSlice className={styles.icon} color="#f8bd11" /> Blog
+            </li>
           </Link>
           <Link href="/contact">
-          <li><FaPizzaSlice color='#f8bd11'/> Contact</li>
+            <li>
+              <FaPizzaSlice className={styles.icon} color="#f8bd11" /> Contact
+            </li>
           </Link>
         </ul>
       </div>
-      <div className={styles.menudetails}>
-        <h2 className={styles.titleMenu}>MENU</h2>
-          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
-          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
-          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
-          <li><FaCaretRight style={styles.icon1}/> Pizzas</li>
-      </div>
-      <div className={styles.brands}>
-        <h2 className={styles.title}> BRANDS</h2>
-        <p className={styles.brand}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <p className={styles.brand}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <p className={styles.brand}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-      <div className={styles.social}>
-        <div className={styles.contact}>
-      <div className={styles.contactmsg}>
-            ORDER NOW
-          </div>
-             <div className={styles.number}>
-              <FaPhoneAlt fontSize={"15px"}/> 444 55 66
-             </div>
-             </div>
-             <div className={styles.socialmedia}>
-               <h2><FaInstagram  className={styles.icon}/></h2>
-               <h2><FaYoutube className={styles.icon}/></h2>
-               <h2><FaFacebook className={styles.icon}/></h2>
-             </div>
-      </div>
-    </div>
-  )
-}
 
-export default Footer
+      <div className={styles.mailForm}>
+        <h2 className={styles.title}> JOIN OUR NEWSLETTER</h2>
+        <form className={styles.mail}>
+          <input
+            type="email"
+            className={styles.input}
+            id="mail"
+            placeholder="Enter your email address"
+          ></input>
+          <button
+            type="button"
+            className={styles.sbmtbtn}
+            onClick={() =>
+              mail.value
+                ? setMsg("Email address registered")
+                : setMsg("Email cannot be left blank")
+            }
+          >
+            SUBMIT NOW
+          </button>
+          <p className={styles.msg}>{msg}</p>
+        </form>
+      </div>
+      <div className={styles.menudetails}>
+        <h2 className={styles.titleMenu}>WORKING HOURS</h2>
+       <p className={styles.hours}>10:00 AM - 10:00 PM</p>
+       <div className={styles.number}>
+          <FaPhoneAlt fontSize={"15px"} /> 444 55 66
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Footer;
--- components/Subscribe.js
+++ components/Subscribe.js
@@ -8,7 +8,7 @@ const Subscribe = () => {
     <div className={styles.container}>
       <div className={styles.subscribe}>
         <h1 className={styles.title}>Subscribe To <span className={styles.title2}>Our Newsletter</span></h1>
-        <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel felis malesuada, tincidunt metus ac, lacinia augue. Donec quis sodales velit. Praesent gravida sit amet ipsum nec hendrerit. Suspendisse placerat justo at ullamcorper pulvinar. Proin imperdiet volutpat hendrerit. Integer in est imperdiet, semper est in, cursus quam. Sed tempus, dui sit amet pretium viverra, risus metus pharetra lorem, sit amet dignissim eros arcu nec lacus.</p>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel felis malesuada, tincidunt metus ac, lacinia augue. </p>
         <div className={styles.mailForm}>
         <form className={styles.mail}>
           <input type='email' className={styles.input} id='mail' placeholder='Enter your email address'>
